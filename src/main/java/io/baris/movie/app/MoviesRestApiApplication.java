54bf97d8e47049fa83da540bc1bfa7a3a389d635
mbaris <mehmetbaris.kalkar@gmail.com>
2017-11-12 16:15:40 +0300

    Initial commit

---
+++ src/main/java/io/baris/movie/app/MoviesRestApiApplication.java
@@ -0,0 +1,12 @@
+package io.baris.movie.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRestApiApplication.class, args);
+	}
+}
